af32891ebae7b752921966b4e28f0765279a6287
Eric Messick <ericdmessick@gmail.com>
2017-03-06 00:39:11 -0600

    speech bubble added

--- src/components/footer.js
+++ src/components/footer.js
@@ -22,9 +22,20 @@ export default class Footer extends Component {
                     <div className="me_container">
                         <img src="" className="footer_me" alt="me" />
                         
-                        <div className="bubble_container">
-                            <p className="black bubbleText">Some text</p>
-                        </div>
+                        <canvas
+                            className="bubble_container"
+                            is="gwd-bezierpath" 
+                            width="773" 
+                            height="470" 
+                            class="gwd-canvas-130s gwd-gen-1y9jgwdanimation" 
+                            anchors="[[[52.5,1.52,0],[52.5,0.5,0],[52.5,0.5,0]],[[382.5,1.5,0],[382.5,2.5,0],[382.5,3.5,0]],[[691.5,1.5,0],[691.5,1.5,0],[691.5,1.5,0]],[[771.5,43.5,0],[772.5,163.5,0],[773.5,283.5,0]],[[634.5,470.51,0],[392.5,469.51,0],[284.35,469.06,0]],[[195.63,437.54,0],[129.54,389.34,0],[124.85,385.92,0]],[[91.85,405.49,0],[68.08,374.08,0],[89.62,383.06,0]],[[106.84,370.92,0],[102.94,367.51,0],[37.32,310.16,0]],[[0.99,236.94,0],[0.5,170.51,0],[-0.5,35.51,0]]]"
+                            closed="" 
+                            stroke-width="1" 
+                            stroke-color="null" 
+                            fill-color="[0,0.4470588235294118,0.7372549019607844,1]" 
+                            geom-type="5">
+                                <p className="black bubbleText">Some text</p>
+                        </canvas>
                         
                     </div>
                 </div>
